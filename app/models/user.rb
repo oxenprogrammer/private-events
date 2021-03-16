b7f7076780c61af397dc243ae1ae6c2cf69b57ab
oxenprogrammer <emmy1000okello@gmail.com>
2021-03-16 16:11:36 +0300

    feat(user): add downcase for consistency

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
+  before_save { self.email = email.downcase }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
   validates :name, presence: true, length: { minimum: 3, maximum: 50 }
   validates :email, presence: true, length: { minimum: 6, maximum: 255 },
-                    format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
+                    format: { with: VALID_EMAIL_REGEX }, uniqueness: true
 end
