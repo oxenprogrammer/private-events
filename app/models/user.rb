d2e25e8bb01a38735d6c02aa21c2c7091e43576a
oxenprogrammer <emmy1000okello@gmail.com>
2021-03-16 15:58:45 +0300

    feat(user): add model validation and test

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,6 @@
 class User < ApplicationRecord
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
+  validates :name, presence: true, length: { minimum: 3, maximum: 50 }
+  validates :email, presence: true, length: { minimum: 6, maximum: 255 },
+                    format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
 end
