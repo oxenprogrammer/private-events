5f63592caf60b1a82cefa47c82a92dff0365fb02
oxenprogrammer <emmy1000okello@gmail.com>
2021-03-18 22:45:49 +0300

    feat(user): add login upon signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,6 +11,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
 
     if @user.save
+      log_in @user
       flash[:success] = 'Welcome to the Private Events!'
       redirect_to @user
     else
@@ -18,31 +19,7 @@ class UsersController < ApplicationController
     end
   end
 
-  def update
-    respond_to do |format|
-      if @user.update(user_params)
-        format.html { redirect_to @user, notice: 'user was successfully updated.' }
-        format.json { render :show, status: :ok, location: @user }
-      else
-        format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @user.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  def destroy
-    @user.destroy
-    respond_to do |format|
-      format.html { redirect_to users_url, notice: 'user was successfully destroyed.' }
-      format.json { head :no_content }
-    end
-  end
-
   private
-
-  def set_user
-    @user = user.find(params[:id])
-  end
 
   def user_params
     params.require(:user).permit(:name, :email, :password, :password_confirmation)
