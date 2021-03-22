c2b7cafa977c4f2a032fbb64b30cc53773860202
oxenprogrammer <emmy1000okello@gmail.com>
2021-03-22 12:00:17 +0300

    feat(controller): add create new event

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,4 +1,23 @@
 class EventsController < ApplicationController
   def new
+    @event = Event.new
+    @user = User.find(current_user.id)
   end
+
+  def create
+    @event = Event.new(event_params)
+
+    @event.creator_id = @user.id
+    if @event.save
+      redirect_to @event
+    else
+      render :new
+    end
+  end
+
+  def event_params
+    params.require(:event).permit(:title, :desc, :venue, :event_date)
+  end
+
+  private :event_params
 end
