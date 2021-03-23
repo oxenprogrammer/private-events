c7df0a32b638f9902b5fee05627dec67e66694ed
oxenprogrammer <emmy1000okello@gmail.com>
2021-03-23 04:10:52 +0300

    feat(invitation): remove log debuggers from controller

--- app/controllers/invitations_controller.rb
+++ app/controllers/invitations_controller.rb
@@ -9,11 +9,8 @@ class InvitationsController < ApplicationController
 
   def create
     @event = Event.find(params[:id])
-    logger.debug "The current event in the log: #{@event}"
     email = params[:email]
-    logger.debug "Invitations in the log: #{params[:email]}"
     old_invitee = User.find_by(email: email)
-    logger.debug "The new_invitee in the log: #{old_invitee}"
 
     if old_invitee
       @invitation = @event.invitations.build(invitee_id: old_invitee.id)
@@ -26,23 +23,7 @@ class InvitationsController < ApplicationController
         render :new
       end
     else
-      @user = User.new(user_params)
-      logger.debug "The new user I am trying to create: #{@user.email}"
-      @user.password = '123456'
-      @user.password_confirmation = '12345'
-
-      if @user.save
-        new_invitee = User.find_by(email: email)
-        @invitation = @event.invitations.build(invitee_id: new_invitee.id)
-        @invitation.event_id = @event.id
-        @invitation.invitee_id = @invitee.id
-
-        if @invitation.save
-          redirect_to event_path(@event.id)
-        else
-          render :new
-        end
-      end
+      render :new
     end
   end
 
@@ -50,9 +31,5 @@ class InvitationsController < ApplicationController
     params.permit(:name, :email, :authenticity_token, :commit, :id)
   end
 
-  def user_params
-    params.permit(:name, :email)
-  end
-
-  private :invitation_params, :user_params
+  private :invitation_params
 end
--- test/controllers/events_controller_test.rb
+++ test/controllers/events_controller_test.rb
@@ -1,7 +1,7 @@
-require "test_helper"
+require 'test_helper'
 
 class EventsControllerTest < ActionDispatch::IntegrationTest
-  test "should get new" do
+  test 'should get new' do
     get events_new_url
     assert_response :success
   end
