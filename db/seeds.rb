94601f8aad7902834254e4200710c95fa6dc397a
oxenprogrammer <emmy1000okello@gmail.com>
2021-03-23 19:25:08 +0300

    feat(database): add seed users to the system

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,14 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+User.create!(
+  [
+    {name: 'Alycia Rojas', email: 'alycia@gmail.com', password: '123456', password_confirmation: '123456'},
+    {name: 'George Shammah', email: 'george@gmail.com', password: '123456', password_confirmation: '123456'},
+    {name: 'Ismael Bruno', email: 'ismael@gmail.com', password: '123456', password_confirmation: '123456'},
+    {name: 'Grace Temi', email: 'temi@gmail.com', password: '123456', password_confirmation: '123456'},
+    {name: 'Abdul Kaliq', email: 'abdul@gmail.com', password: '123456', password_confirmation: '123456'},
+    {name: 'Jordan Kaweesi', email: 'ordon@gmail.com', password: '123456', password_confirmation: '123456'},
+    {name: 'Akello Joan', email: 'joan@gmail.com', password: '123456', password_confirmation: '123456'},
+    {name: 'Elizabeth Kirabo', email: 'liz@gmail.com', password: '123456', password_confirmation: '123456'},
+    {name: 'Agatha Gwth', email: 'agatha@gmail.com', password: '123456', password_confirmation: '123456'},
+    {name: 'Suzan Oboo', email: 'suzan@gmail.com', password: '123456', password_confirmation: '123456'},
+  ]
+)
