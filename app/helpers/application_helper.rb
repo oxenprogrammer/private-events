eb2cf65e7f9dcecdcbe1e284de49c7e2a3f12dfa
oxenprogrammer <emmy1000okello@gmail.com>
2021-03-24 07:15:08 +0300

    hotfix(review): fix navbar-brand name link

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -14,4 +14,12 @@ module ApplicationHelper
   def navbar_link_login
     link_to 'Login', login_path, class: 'nav-link' unless logged_in?
   end
+
+  def navbar_link_home
+    if logged_in?
+      link_to 'Private Events', user_url(current_user.id), class: 'navbar-brand'
+    else
+      link_to 'Private Events', root_url, class: 'navbar-brand'
+    end
+  end
 end
